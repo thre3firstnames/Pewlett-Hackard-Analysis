5dda6e5d2da6d144e0abd65ec59c3fd7d25b5cc4
thre3firstnames <natkmerry@gmail.com>
2022-01-31 06:56:28 -0700

    schmea update for titles

--- Analysis_Projects_Folder/schema.sql
+++ Analysis_Projects_Folder/schema.sql
@@ -35,6 +35,17 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
+
+SELECT * FROM departments;
+SELECT * FROM employees;
+SELECT * FROM dept_manager;
+SELECT * FROM salaries;
+
+SELECT * FROM dept_emp;
+SELECT * FROM titles;
+
+DROP Table dept_emp;
+
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
 	dept_no VARCHAR(4) NOT NULL,
@@ -42,16 +53,20 @@ CREATE TABLE dept_emp (
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, dept_no)
 );
+
+SELECT * FROM dept_emp;
+SELECT * FROM titles;
+
+DROP TABLE titles;
 
 CREATE TABLE titles (     
 	emp_no INT NOT NULL,
-	title VARCHAR(40) NOT NULL,
+	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title, from_date)
 );
 
-SELECT * FROM departments;
