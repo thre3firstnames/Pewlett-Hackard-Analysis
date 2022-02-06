f6f8622f0166166aec3b4e0ddab8e03e050c15ae
thre3firstnames <natkmerry@gmail.com>
2022-02-05 17:29:09 -0700

    Deliv 2 completed

--- Analysis_Projects_Folder/Pewlett-Hackard-Analysis-Folder/Queries/queries.sql
+++ Analysis_Projects_Folder/Pewlett-Hackard-Analysis-Folder/Queries/queries.sql
@@ -247,21 +247,6 @@ CREATE TABLE titles (
 -- WHERE d.dept_name IN ('Sales','Development');
 
 
-SELECT DISTINCT ON (e.emp_no) e.emp_no,
-e.first_name,
-e.last_name,
-e.birth_date,
-de.from_date,
-de.to_date,
-ti.title
-FROM employees as e 
-INNER JOIN dept_emp as de
-ON de.emp_no = e.emp_no
-INNER JOIN titles as ti
-ON e.emp_no = ti.emp_no
-WHERE (de.to_date = '9999-01-01')
-AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
-ORDER BY emp_no;
 
 CREATE TABLE titles (     
 	emp_no INT NOT NULL,
