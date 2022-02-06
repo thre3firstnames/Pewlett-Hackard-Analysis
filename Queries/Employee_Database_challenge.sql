13412d9bce00ed4a154f57e09c222189a8fbabdb
thre3firstnames <natkmerry@gmail.com>
2022-02-05 18:50:13 -0700

    ReadME Edits 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,11 +1,3 @@
--- From employeees, retrieve (emp_no, first_name, last_name) - check
--- From titles retrieve (title, from_date, to_date) - check
--- Create a new Table with INTO -check
--- Join the tables on the PK - check
--- filer data from the 'birth_date' column to retrieve employees born between 1952-1955 - check
--- order by employee number -- check
--- Export the retirement_titles table as retirement_titles.csv -- check
-
 -- Number of Retiring Employees by Title
 SELECT e.emp_no,
 	e.first_name,
@@ -20,14 +12,7 @@ ON (e.emp_no = ti.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
 
--- retrieve emp_no, first, last, and title FROM retirement_titles -- check
--- Use Distinct on to retrieve the first occurance of the employee number. -- check
--- exclude employees who have already left the company by using to_date -- check
--- create a unique table -- check
--- sort the unique table in ascending order by empployee number and descending order by the last date of the most recent title -- check
--- export unique titles -- check
-
--- Use Dictinct with Orderby to remove duplicate rows from retirementt_titles
+-- Use Dictinct on with Orderby to remove duplicate rows from retirementt_titles
 SELECT DISTINCT ON (emp_no) emp_no,
 	first_name,
 	last_name,
@@ -37,15 +22,27 @@ FROM retirement_titles
 WHERE to_date = '9999-01-01'
 ORDER BY emp_no ASC, title DESC;
 
--- retrieve number of employees by their most recent job title who are about to retire -- check
--- retrieve the number of titles from the Unique titles table -- check
--- create a retiring titles table -- check
--- group the table by title then sort the count column in desc order -- check
--- export the retiring titles retiring_titles.csv -- check
-
 -- Count number of unique titles, order by descending count
 SELECT COUNT(title), title
 INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(title) DESC;
+
+--Create mentorship eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+ti.title
+INTO mentorship_eligibilty
+FROM employees as e 
+INNER JOIN dept_emp as de
+ON de.emp_no = e.emp_no
+INNER JOIN titles as ti
+ON e.emp_no = ti.emp_no
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
--- README.md
+++ README.md
@@ -1,10 +1,23 @@
 # Employee Database Analysis for Pewlett Hackard using SQL
-## Overview: Explain the purpose of this analysis.
+## Overview
+Provide PH with datasets built from their employee records that illustrate the number of current employees reaching retirement age, and assemble the data for a list of current, eligible employees for participation in an upcoming mentorship program.
 
 ## Results
  Provide a bulleted list with four major points from the two analysis deliverables. Use images as support where needed.
+- According to the data resulting from our examination of retiring employees, almost 25% of PH’s current workforce will be retiring soon. This is a staggering number even against PH’s large current employee total – **72,458 employees out of 300,024**.
+- Illustrated by the output pictured below, a majority (**70%**) of retiring employees hold *Senior* positions within PH.
+![retirement_titles.png](/Data/retirement_titles.png)
+- three
+- four
+
+(one for each table, let’s say) 
+
 
 ## Summary
 Provide high-level responses to the following questions, then provide two additional queries or tables that may provide more insight into the upcoming "silver tsunami."
-How many roles will need to be filled as the "silver tsunami" begins to make an impact?
-Are there enough qualified, retirement-ready employees in the departments to mentor the next generation of Pewlett Hackard employees?
+
+- How many roles will need to be filled as the "silver tsunami" begins to make an impact?
+- Are there enough qualified, retirement-ready employees in the departments to mentor the next generation of Pewlett Hackard employees?
+
+- Digging deeper into the departmental splits of the current employees approaching retirement might provide further insight into which departments need special support. At least two additional tables will need to be created for this analysis–one for retiring employees by department and one for non-retiring employees by department. Comparing these two tables will 
+- 
